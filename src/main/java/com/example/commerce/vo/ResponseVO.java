b9ec4b7a58c3636b391d42cc320e2871247e77eb
jupark <jonguk1221@gmail.com>
2017-11-22 18:58:03 +0900

    기본 요청/응답 VO 추가

---
+++ src/main/java/com/example/commerce/vo/ResponseVO.java
@@ -0,0 +1,34 @@
+package com.example.commerce.vo;
+
+import com.example.commerce.common.ResultCodes;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@ToString
+public class ResponseVO implements Serializable {
+
+  @JsonProperty
+  private String resultCode;
+
+  @JsonProperty
+  private String message;
+
+  public ResponseVO(String resultCode) {
+    this.resultCode = resultCode;
+  }
+
+  public ResponseVO(String resultCode, String message) {
+    this.resultCode = resultCode;
+    this.message = message;
+  }
+
+  public static ResponseVO ok() {
+    return new ResponseVO(ResultCodes.OK);
+  }
+}
