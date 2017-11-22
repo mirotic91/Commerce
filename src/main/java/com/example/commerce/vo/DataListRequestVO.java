7c7690b39749d2fcb64cc6583adcd63119d5643d
jupark <jonguk1221@gmail.com>
2017-11-22 18:58:38 +0900

    데이터 요청/응답 VO 추가

---
+++ src/main/java/com/example/commerce/vo/DataListRequestVO.java
@@ -0,0 +1,29 @@
+package com.example.commerce.vo;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.querydsl.QPageRequest;
+
+import lombok.Setter;
+
+@Setter
+public class DataListRequestVO extends RequestVO {
+
+  private static final int DEFAULT_PAGE_ROW = 20;
+
+  private int page;
+
+  private int pageSize;
+
+  public int getPage() {
+    return page < 1 ? 1 : page;
+  }
+
+  public int getPageSize() {
+    return pageSize <= 0 ? DEFAULT_PAGE_ROW : pageSize;
+  }
+
+  public Pageable getPageable() {
+    return new QPageRequest(getPage() - 1, getPageSize());
+  }
+
+}
