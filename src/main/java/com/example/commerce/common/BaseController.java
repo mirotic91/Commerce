d97f51a5e2ba14211bcddd8d0706179ff9707fc9
jupark <jonguk1221@gmail.com>
2018-03-08 13:18:14 +0900

    SecurityUtil 로그인 유저 가져오기 정정

--- src/main/java/com/example/commerce/common/BaseController.java
+++ src/main/java/com/example/commerce/common/BaseController.java
@@ -1,12 +1,11 @@
 package com.example.commerce.common;
 
-import com.example.commerce.model.User;
 import com.example.commerce.util.SecurityUtil;
 
 public class BaseController {
 
-  protected User getCurrentUser() {
-    return SecurityUtil.getCurrentUser();
+  protected Long getCurrentUserId() {
+    return SecurityUtil.getCurrentUserDetails().getId();
   }
 
 }
