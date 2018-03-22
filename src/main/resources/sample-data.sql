fc6705895b37ff861e0500cdda32984f5b9aa96e
jupark <jonguk1221@gmail.com>
2018-03-22 22:18:01 +0900

    장바구니 관련 샘플데이터 추가

--- src/main/resources/sample-data.sql
+++ src/main/resources/sample-data.sql
@@ -37,3 +37,26 @@ INSERT INTO t_product (id,color,name,price,created_date) VALUES (-19,'orange','m
 INSERT INTO t_product (id,color,name,price,created_date) VALUES (-20,'navy','max97',99.9,'2017-06-06 11:32:32');
 -- t_product END
 
+-- t_cart START
+INSERT INTO t_cart (id,created_date) VALUES (-1,'2017-12-16 01:12:44');
+INSERT INTO t_cart (id,created_date) VALUES (-2,'2017-12-11 11:32:14');
+-- t_cart END
+
+-- t_cart_product START
+INSERT INTO t_cart_product (id,cart_id,product_id,buy_count,created_date) VALUES (-1,-1,-1,3,'2017-12-16 01:12:44');
+INSERT INTO t_cart_product (id,cart_id,product_id,buy_count,created_date) VALUES (-2,-1,-2,5,'2017-12-11 11:32:14');
+INSERT INTO t_cart_product (id,cart_id,product_id,buy_count,created_date) VALUES (-3,-1,-3,1,'2017-12-16 01:12:45');
+INSERT INTO t_cart_product (id,cart_id,product_id,buy_count,created_date) VALUES (-4,-1,-4,1,'2017-12-11 11:32:15');
+INSERT INTO t_cart_product (id,cart_id,product_id,buy_count,created_date) VALUES (-5,-1,-5,'12','2017-12-16 01:12:46');
+INSERT INTO t_cart_product (id,cart_id,product_id,buy_count,created_date) VALUES (-6,-1,-6,2,'2017-12-11 11:32:16');
+INSERT INTO t_cart_product (id,cart_id,product_id,buy_count,created_date) VALUES (-7,-2,-1,'1','2017-12-16 01:12:47');
+INSERT INTO t_cart_product (id,cart_id,product_id,buy_count,created_date) VALUES (-8,-2,-2,'4','2017-12-11 11:32:17');
+INSERT INTO t_cart_product (id,cart_id,product_id,buy_count,created_date) VALUES (-9,-2,-3,'2','2017-12-16 01:12:48');
+INSERT INTO t_cart_product (id,cart_id,product_id,buy_count,created_date) VALUES (-10,-2,-10,'3','2017-12-11 11:32:18');
+INSERT INTO t_cart_product (id,cart_id,product_id,buy_count,created_date) VALUES (-11,-2,-11,'1','2017-12-16 01:12:49');
+INSERT INTO t_cart_product (id,cart_id,product_id,buy_count,created_date) VALUES (-12,-2,-12,'5','2017-12-11 11:32:19');
+INSERT INTO t_cart_product (id,cart_id,product_id,buy_count,created_date) VALUES (-13,-2,-13,'3','2017-12-16 01:12:50');
+INSERT INTO t_cart_product (id,cart_id,product_id,buy_count,created_date) VALUES (-14,-2,-14,'2','2017-12-11 11:32:20');
+INSERT INTO t_cart_product (id,cart_id,product_id,buy_count,created_date) VALUES (-15,-2,-15,'5','2017-12-16 01:12:51');
+-- t_cart_product END
+
